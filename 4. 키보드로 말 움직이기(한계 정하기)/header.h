fb499c8d32c8d89a4d9305c6e11fa96bf169f69d
Kim Hyunseo <79328858+hs11015@users.noreply.github.com>
2022-05-31 20:19:04 +0900

    Add files via upload

---
+++ 4. 키보드로 말 움직이기(한계 정하기)/header.h
@@ -0,0 +1,24 @@
+// 모각코 아이두 C 3월과정 코딩뿌시기
+
+// header.h
+
+#ifndef HEADER
+#define HEADER
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <windows.h>
+#include <conio.h>
+
+#define LEFT 75    // 방향키는
+#define RIGHT 77   // 확장키(2바이트)
+#define UP 72      // 이게 각자의
+#define DOWN 80    // 고유값
+#define ARROW 224  // 이건 방향키 값
+
+void GotoXY(int x, int y);
+void CursorView(char show);
+void move_pointer();
+
+#endif
---
+++ 4. 키보드로 말 움직이기(한계 정하기)/main.c
@@ -0,0 +1,8 @@
+// main.c
+
+#include "header.h"
+int main() {
+	CursorView(0);
+	move_pointer();
+	return 0;
+}
