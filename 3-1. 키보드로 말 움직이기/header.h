58dfeb5bffde79183a6be86d42c8129a1c219ba0
Kim Hyunseo <79328858+hs11015@users.noreply.github.com>
2022-05-31 20:17:01 +0900

    Add files via upload

---
+++ 3-1. 키보드로 말 움직이기/header.h
@@ -0,0 +1,24 @@
+/* 모각코 아이두C 3월 과정 미로찾기 실습 1 코딩뿌시기 */
+
+// header.h
+
+#ifndef HEADER
+#define HEADER     //header파일 정의
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <Windows.h>    // 윈도우 함수 헤더파일
+
+#define SIZE 19         // 매크로를 사용해 미로의 크기 정해두기 위함
+#define XP 40
+#define YP 5
+
+void LoadMaze(char num);
+void GotoXY(int x, int y);
+void PrintMazeGame();
+void CursorView(char show);
+
+char maze[SIZE][SIZE];
+
+#endif
