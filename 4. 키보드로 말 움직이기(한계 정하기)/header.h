a11fc98614265aab09143fba26d97d11b6bbfe85
Kim Hyunseo <79328858+hs11015@users.noreply.github.com>
2022-06-01 15:15:48 +0900

    Update header.h

--- 4. 키보드로 말 움직이기(한계 정하기)/header.h
+++ 4. 키보드로 말 움직이기(한계 정하기)/header.h
@@ -1,4 +1,3 @@
-// ¸ğ°¢ÄÚ ¾ÆÀÌµÎ C 3¿ù°úÁ¤ ÄÚµù»Ñ½Ã±â
 
 // header.h
 
@@ -11,11 +10,11 @@
 #include <windows.h>
 #include <conio.h>
 
-#define LEFT 75    // ¹æÇâÅ°´Â
-#define RIGHT 77   // È®ÀåÅ°(2¹ÙÀÌÆ®)
-#define UP 72      // ÀÌ°Ô °¢ÀÚÀÇ
-#define DOWN 80    // °íÀ¯°ª
-#define ARROW 224  // ÀÌ°Ç ¹æÇâÅ° °ª
+#define LEFT 75    // ë°©í–¥í‚¤ëŠ”
+#define RIGHT 77   // í™•ì¥í‚¤(2ë°”ì´íŠ¸)
+#define UP 72      // ì´ê²Œ ê°ìì˜
+#define DOWN 80    // ê³ ìœ ê°’
+#define ARROW 224  // ì´ê±´ ë°©í–¥í‚¤ ê°’
 
 void GotoXY(int x, int y);
 void CursorView(char show);
